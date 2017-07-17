80ca8e6cc3c7ca926906662ff9fe549da98c9ed4
Skyridge Devcamp <skyridge@devcamp.com>
2017-07-17 16:35:32 +0000

    Dev tool initial commit

---
+++ lib/brannum_view_tool.rb
@@ -0,0 +1,6 @@
+require "brannum_view_tool/version"
+require "brannum_view_tool/renderer"
+
+module BrannumViewTool
+  # Your code goes here...
+end
---
+++ lib/brannum_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BrannumViewTool
+  VERSION = "0.1.0"
+end
